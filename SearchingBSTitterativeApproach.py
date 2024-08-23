c787e1915e30194535db674ddef11818cc54f83a
KalebYigezu <43226925+KalebYigezu@users.noreply.github.com>
2024-08-23 12:08:15 -0700

    Update SearchingBSTitterativeApproach.py

--- SearchingBSTitterativeApproach.py
+++ SearchingBSTitterativeApproach.py
@@ -1,1 +1,33 @@
+class Node:
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
 
+
+def search(root, key):
+    while root is not None:
+
+        if key > root.key:
+            root = root.right
+        elif key < root.key:
+            root = root.left
+
+        else:
+            return root
+
+    return None
+
+
+# Driver Code
+if __name__ == "__main__":
+    root = Node(50)
+    root.left = Node(30)
+    root.right = Node(70)
+    root.left.left = Node(20)
+    root.left.right = Node(40)
+    root.right.left = Node(60)
+    root.right.right = Node(80)
+
+    print("Found" if search(root, 19) else "Not Found")
+    print("Found" if search(root, 80) else "Not Found")
